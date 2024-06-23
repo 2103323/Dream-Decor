aba02201f1373ae6f7eb14a5257f9eb1343d3d2c
suyash <suyashjadaun@gmail.com>
2024-06-24 00:01:56 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,25 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
- 
-export default clerkMiddleware({
-  publicRoutes: [
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
     '/',
-    '/events/:id',
+    '/events/:id', // Assuming you want this route to be publicly accessible
     '/api/webhook/clerk',
     '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
+    '/api/uploadthing']);
+
+    const isIgnoredRoute = createRouteMatcher([    
+      '/api/webhook/clerk',
+      '/api/webhook/stripe',
+      '/api/uploadthing'
+    ]);
+    export default clerkMiddleware((auth, req) => {
+      // If it's a public route, do nothing
+      if (isPublicRoute(req)) return;
+      if (isIgnoredRoute(req)) return;
+     // For any other route, require authentication
+      auth().protect();
 });
- 
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
