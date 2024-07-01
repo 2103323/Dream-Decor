73a6764f0cb3295ec54ece3c5b8f5bbcde56fb36
Utsav Bansal <utsavbansal75@gmail.com>
2024-07-02 01:37:41 +0530

    contact form link with emailjs

--- components/shared/ContactForm.tsx
+++ components/shared/ContactForm.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+
+
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -18,109 +24,194 @@ import { eventFormSchema } from "@/lib/validator";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea"
 export default function ContactForm() {
-  const [fullname, setFullname] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [error, setError] = useState([]);
-  const [success, setSuccess] = useState(false);
-  const form = useForm<z.infer<typeof eventFormSchema>>({
-    resolver: zodResolver(eventFormSchema),
-    // defaultValues: initialValues,
-  });
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    console.log("Full name: ", fullname);
-    console.log("Email: ", email);
-    console.log("Message: ", message);
-
-    const res = await fetch("api/contact", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({
-        fullname,
-        email,
-        message,
-      }),
-    });
-
-    const { msg, success } = await res.json();
-    setError(msg);
-    setSuccess(success);
-
-    if (success) {
-      setFullname("");
-      setEmail("");
-      setMessage("");
-    }
+    emailjs
+      .sendForm('service_ik9qjus', 'template_r9atm34', form.current, {
+        publicKey: 'OchG3fTlGm5-QuV7t',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert("Message Sent Successfully!!");
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          alert("Unable to sent the message!!");
+        },
+      );
   };
 
   return (
-      <Form {...form} >
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5" >
-          <div className="flex flex-col gap-5 md:flex-row">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormControl>
-                    <Input
-                      placeholder="Your Name"
-                      {...field}
-                      className="input-field"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormControl>
-                    <Input
-                      placeholder="Your Email"
-                      {...field}
-                      className="input-field"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="flex flex-col gap-5 md:flex-row">
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormControl className="h-72">
-                    <Textarea
-                      placeholder="Description"
-                      {...field}
-                      className="textarea rounded-2xl"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button 
-          type="submit"
-          size="lg"
-          disabled={form.formState.isSubmitting}
-          className="button col-span-2 w-full"
-        >
-          {form.formState.isSubmitting ? (
-            'Submitting...'
-          ): `Send`}</Button>
-        </form>
-      </Form>
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-5" >
+      <label className="w-full">Name</label>
+      <input type="text" name="user_name" className="input-field" />
+      <label className="w-full">Email</label>
+      <input type="email" name="user_email" className="input-field" />
+      <label className="w-full">Message</label>
+      <textarea name="message" className="textarea rounded-2xl" />
+      <input type="submit" value="Send" className="button col-span-2 w-full" />
+    </form> 
   )};
+
+  // const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs
+  //     .sendForm('service_ik9qjus', 'template_r9atm34', form.current, {
+  //       publicKey: 'OchG3fTlGm5-QuV7t',
+  //     })
+  //     .then(
+  //       () => {
+  //         console.log('SUCCESS!');
+  //       },
+  //       (error) => {
+  //         console.log('FAILED...', error.text);
+  //       },
+  //     );
+  // };
+
+
+
+{/* <form ref={form} onSubmit={sendEmail}>
+    <label>Name</label>
+    <input type="text" name="user_name" />
+    <label>Email</label>
+    <input type="email" name="user_email" />
+    <label>Message</label>
+    <textarea name="message" />
+    <input type="submit" value="Send" />
+  </form>  */}
+
+
+// "use client";
+
+// import { useState } from "react";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { z } from "zod";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { eventFormSchema } from "@/lib/validator";
+// import { Input } from "@/components/ui/input";
+// import { Textarea } from "@/components/ui/textarea"
+// export default function ContactForm() {
+//   const [fullname, setFullname] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [message, setMessage] = useState("");
+//   const [error, setError] = useState([]);
+//   const [success, setSuccess] = useState(false);
+//   const form = useForm<z.infer<typeof eventFormSchema>>({
+//     resolver: zodResolver(eventFormSchema),
+//     // defaultValues: initialValues,
+//   });
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+
+//     console.log("Full name: ", fullname);
+//     console.log("Email: ", email);
+//     console.log("Message: ", message);
+
+//     const res = await fetch("api/contact", {
+//       method: "POST",
+//       headers: {
+//         "Content-type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         fullname,
+//         email,
+//         message,
+//       }),
+//     });
+
+//     const { msg, success } = await res.json();
+//     setError(msg);
+//     setSuccess(success);
+
+//     if (success) {
+//       setFullname("");
+//       setEmail("");
+//       setMessage("");
+//     }
+//   };
+
+//   return (
+//       <Form {...form} >
+//         <form onSubmit={handleSubmit} className="flex flex-col gap-5" >
+//           <div className="flex flex-col gap-5 md:flex-row">
+//             <FormField
+//               control={form.control}
+//               name="name"
+//               render={({ field }) => (
+//                 <FormItem className="w-full">
+//                   <FormControl>
+//                     <Input
+//                       placeholder="Your Name"
+//                       {...field}
+//                       className="input-field"
+//                     />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+//             <FormField
+//               control={form.control}
+//               name="email"
+//               render={({ field }) => (
+//                 <FormItem className="w-full">
+//                   <FormControl>
+//                     <Input
+//                       placeholder="Your Email"
+//                       {...field}
+//                       className="input-field"
+//                     />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+//           </div>
+//           <div className="flex flex-col gap-5 md:flex-row">
+//             <FormField
+//               control={form.control}
+//               name="description"
+//               render={({ field }) => (
+//                 <FormItem className="w-full">
+//                   <FormControl className="h-72">
+//                     <Textarea
+//                       placeholder="Description"
+//                       {...field}
+//                       className="textarea rounded-2xl"
+//                     />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+//           </div>
+//           <Button 
+//           type="submit"
+//           size="lg"
+//           disabled={form.formState.isSubmitting}
+//           className="button col-span-2 w-full"
+//         >
+//           {form.formState.isSubmitting ? (
+//             'Submitting...'
+//           ): `Send`}</Button>
+//         </form>
+//       </Form>
+//   )};
